dec744b7b1104d343de07480c0036a7b778a7555
Kyle Grier <grier@kth.se>
2018-02-10 11:58:08 -0600

    changes to test

--- CarND-Behavioral-Cloning-P3/modeling.py
+++ CarND-Behavioral-Cloning-P3/modeling.py
@@ -28,11 +28,11 @@ def nvidiaModel():
         input_shape=(ch, row, col),\
         output_shape=(ch, row, col)))
 	#model.add(Lambda(x: x/127.5 - 1, input_shape=(160, 320, 3)))
-	model.add(Convolution2D(24, 5, 5, strides=(2, 2), activation='relu'))
-	model.add(Convolution2D(36, 5, 5, strides=(2, 2), activation='relu'))
-	model.add(Convolution2D(48, 5, 5, strides=(2, 2), activation='relu'))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
-	model.add(Convolution2D(64, 3, 3, activation='relu'))
+	model.add(Convolution2D(24, 5, strides=(2, 2), activation='relu'))
+	model.add(Convolution2D(36, 5, strides=(2, 2), activation='relu'))
+	model.add(Convolution2D(48, 5, strides=(2, 2), activation='relu'))
+	model.add(Convolution2D(64, 3, activation='relu'))
+	model.add(Convolution2D(64, 3, activation='relu'))
 
 	model.add(Flatten())
 
